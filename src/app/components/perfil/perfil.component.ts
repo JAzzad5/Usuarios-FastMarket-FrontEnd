ca8f94358b29e6152409223b3895fa03eae130d7
JAzzad5 <carlosjosue0114@gmail.com>
2021-10-28 00:29:46 -0600

    [ADD] añadir algunos routerlink para interaccion

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-perfil',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-
+  faArrowLeft=faArrowLeft;
   constructor() { }
 
   ngOnInit(): void {
