54626ef3c4a48f8ddfb016284edd2fc932648a31
JAzzad5 <carlosjosue0114@gmail.com>
2021-11-05 19:51:04 -0600

    [ADD] numero de productos en el carrito

--- src/app/components/barra-superior/barra-superior.component.ts
+++ src/app/components/barra-superior/barra-superior.component.ts
@@ -12,16 +12,18 @@ export class BarraSuperiorComponent implements OnInit {
   faUser = faUser;
   faMapMarkerAlt = faMapMarkerAlt;
   faShoppingCart = faShoppingCart;
-
+  
   constructor(private modalService:NgbModal, private usuarioService:UsuariosService) { }
   NombreUsuario:any;
   CarritoUsuario:any;
+  ProductosCarrito:any;
   ngOnInit(): void {
     this.usuarioService.obtenerUsuario("61784e12a85334e2f36e9a95").subscribe(
       res=>{
         console.log(res);
         this.NombreUsuario = res.NombreUsuario;
         this.CarritoUsuario = res.CarritoCompras;
+        this.ProductosCarrito = res.CarritoCompras.length;
         console.log("Carrito",this.CarritoUsuario);
       },
       error=>{
